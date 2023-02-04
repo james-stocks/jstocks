5bba3ef4fa260c655c4ec005bcae78de6ec7030c
James Stocks <jstocks@gmail.com>
2023-02-04 23:58:52 +0000

    Initial page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>jstocks.online</h1>
+      <div className="App-blurb">
+          My personal website!<br/>
+          <br/>
+          <h2>Links:</h2>
+          <a href="https://www.linkedin.com/in/james-stocks-5316b337/">LinkedIn</a><br/>
+          <a href="https://twitter.com/_JamesStocks">Twitter</a><br/>
+          <a href="https://68st0x20.itch.io/">Itch</a> - several games you can play in browser!<br/>
+          <a href="https://github.com/james-stocks">Github</a> (not much activity because I use Gitlab for personal projects and a separate account for work)<br/>
+          <a href="https://gitlab.com/68K">Gitlab</a><br/>
+          Things I've written on <a href="https://medium.com/@jstocks">medium.com</a>
+      </div>
+      <footer className="footer">
+        <p className="text-footer">
+            Copyright © 2023 James Stocks | <a href="https://github.com/james-stocks/jstocks">Source</a> | <a href="mailto:jstocks@duck.com">jstocks@duck.com</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
