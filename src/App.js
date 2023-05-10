b971240a1441a6989d88093c3c480e793b7e1763
James Stocks <jstocks@gmail.com>
2023-05-10 20:37:18 +0000

    Update main page slightly

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
     <div className="App">
       <h1>jstocks.online</h1>
       <div className="App-blurb">
-          <b>My personal website!</b><br/>
+          <b><i>My little space on the Internet</i></b><br/>
           <h2>Links:</h2>
           <a href="https://www.linkedin.com/in/james-stocks-5316b337/">LinkedIn</a><br/>
           <a href="https://twitter.com/_JamesStocks">Twitter</a><br/>
           <a href="https://68st0x20.itch.io/">Itch</a> - several games you can play in browser!<br/>
-          <a href="https://github.com/james-stocks">Github</a> (not much here - I use GitLab for personal projects and a private account for work)<br/>
+          <a href="https://github.com/james-stocks">Github</a><br/>
           <a href="https://gitlab.com/68K">GitLab</a><br/>
           Things I've written on <a href="https://medium.com/@jstocks">medium.com</a>
       </div>
